1004aee97b9f648da9a6d5d2217cdddb90a52545
Jonathan David Shelley <jdshelley@gmail.com>
2015-01-05 06:26:41 -0500

    uploading finished changes
    the finished product....I still need to go back and clean up code and
    tidy up the csss

--- app.js
+++ app.js
@@ -137,7 +137,7 @@ function validateSquares(board){
 }
 
 function createBoard(){
-    return [[5, 0, 4, 0, 7, 0, 9, 0, 0],
+    /*return [[5, 0, 4, 0, 7, 0, 9, 0, 0],
         [6, 0, 2, 1, 9, 0, 3, 4, 0],
         [1, 9, 0, 3, 4, 0, 5, 6, 7],
         [8, 0, 9, 0, 6, 1, 4, 0, 3],
@@ -145,14 +145,27 @@ function createBoard(){
         [0, 0, 0, 0, 0, 0, 0, 0, 0],
         [9, 0, 1, 0, 3, 7, 0, 8, 4],
         [2, 0, 7, 0, 1, 0, 6, 0, 5],
-        [0, 4, 0, 2, 0, 6, 1, 0, 0]];
+        [0, 4, 0, 2, 0, 6, 1, 0, 0]];*/
+
+  return[  [5, 3, 4, 6, 7, 8, 9, 1, 2],
+        [6, 7, 2, 1, 9, 5, 3, 4, 8],
+        [1, 9, 8, 3, 4, 2, 5, 6, 7],
+        [8, 5, 9, 7, 6, 1, 4, 2, 3],
+        [4, 2, 6, 8, 5, 3, 7, 9, 1],
+        [7, 1, 3, 9, 2, 4, 8, 5, 6],
+        [9, 6, 1, 5, 3, 7, 2, 8, 4],
+        [2, 8, 7, 4, 1, 9, 6, 3, 5],
+        [3, 4, 5, 2, 8, 6, 1, 7, 0]];
 }
 
 
 
 function validSolution(){
     var board = getHtmlBoard();
+
+    //console.log(board);
     var msg;
+
     if(totalCount(board) && validateHorizontal(board) && validateVertical(board) && validateSquares(board)){
         msg = 'Congratulations you completed the board!!'
     }else{
@@ -169,7 +182,7 @@ function getHtmlBoard(){
            if(document.getElementById(i +'-' + j).value === ''){
                innerArray.push(0);
            }else{
-               innerArray.push(document.getElementById(i +'-' + j).value);
+               innerArray.push(parseInt(document.getElementById(i +'-' + j).value, 10));
            }
        }
        outerArray.push(innerArray);
