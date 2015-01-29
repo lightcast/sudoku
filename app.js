5dfe4f15a0b2e175bcf6bc3a28ef0db1d89201c5
Jonathan Shelley <jdshelley@gmail.com>
2015-01-29 17:48:16 -0500

    removed unused code and fixed some functions

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ function tableCreate(newBoard) {
             var rightColor = 0;
             for(var y =1; y <=9; y++){
                 rightColor += 1;
-                //var cellID = parseInt(  (x + y));
+
                 var cell = document.getElementById('sudokuTable').appendChild(document.createElement('div'));
 
                 var inputCellID  = x + '-' + y;
@@ -23,9 +23,6 @@ function tableCreate(newBoard) {
                     cell.innerHTML =  "<input type='text' id=\'" + inputCellID + "'\ class='inputCell' maxlength='1' value='' keydown='return (event.charCode >= 48 && event.charCode <= 57) || (event.charCode === 46) || (event.charCode === 8);'/>";
 
                 }
-
-
-
 
                 cell.style.fontSize = '35px';
                 cell.style.textAlign = 'center';
@@ -50,16 +47,13 @@ function tableCreate(newBoard) {
                 // we finish styling the grid
                 cell.style.width = '55px';
                 cell.style.height = '55px';
-                //cell.setAttribute('id', 'div');
-
-                //console.log(cell);
             }
         }
 }
 tableCreate(createBoard());
 
 function createBoard(){
-    return  [[5, 0, 4, 0, 7, 0, 9, 0, 0],
+    return [[5, 0, 4, 0, 7, 0, 9, 0, 0],
         [6, 0, 2, 1, 9, 0, 3, 4, 0],
         [1, 9, 0, 3, 4, 0, 5, 6, 7],
         [8, 0, 9, 0, 6, 1, 4, 0, 3],
@@ -72,93 +66,35 @@ function createBoard(){
 }
 
 
-
-function totalCount(board){
-    var totalCount = 0;
-    for(row in board){
-        var count = 0;
-        for(var i = 0; i < board[row].length ; i++ ){
-            if(board[row][i] === 0){
-                return false;
-            }else{
-                count += board[row][i];
-            }
-        }
-        if(count !== 45){
-            return false;
-        }else{
-            totalCount += count;
-        }
-    }
-    return (totalCount === 405); // total count of the entire board should equal 405
+function equals45(n){
+    return n == 45;
 }
 
-
-function validateHorizontal(board){
-    for(var outer = 0; outer < 9; outer++){
-        var value = 0;
-        for(var inner = 0; inner < 9; inner++){
-            value += board[outer][inner];
+function checkSolution(board){
+    var sumHorizontal = [0,0,0,0,0,0,0,0,0];
+    var sumVertical = [0,0,0,0,0,0,0,0,0];
+    gridSum = [[0,0,0],[0,0,0],[0,0,0]];
+    for (var i=0;i<9;i++){
+        for (var j=0;j<9;j++){
+            sumHorizontal[i] += board[i][j];
+            sumVertical[j] += board[i][j];
+            gridSum[Math.floor((i/3))][Math.floor(j/3)] += board[i][j];
         }
-        if(value !== 45){
+    }
+    for (var i=0;i<3;i++){
+        if(!gridSum[i].every(equals45)){
             return false;
         }
+
     }
-    return true;
+    return (sumHorizontal.every(equals45) && sumVertical.every(equals45));
 }
-
-
-function validateVertical(board){
-    for(var b in board){
-        for(var inner = 0; inner < 9; inner++){
-            var value = 0;
-            for(var outer = 0; outer < 9; outer++){
-                value += board[outer][inner];
-            }
-            if(value !== 45){
-                return false;
-            }
-        }
-    }
-    return true;
-}
-
-
-function validateSquares(board){
-    var rowPosition = 0;
-    var columnPosition = 0;
-    for(var b = 0; b < board.length -1; b++){
-        var value = 0;
-        var columnEnd = columnPosition + 3;
-        var rowEnd = rowPosition + 3;
-        for(; rowPosition < rowEnd; rowPosition++){
-            for(; columnPosition < columnEnd; columnPosition++){
-                value += board[columnPosition][rowPosition];
-
-            }
-            columnPosition = columnEnd - 3;
-        }
-        if(value !== 45){
-            return false;
-        }
-        if(rowEnd === 9){
-            rowPosition = 0;
-        }
-    }
-    return true;
-}
-
 
 
 function validSolution(){
     var board = getHtmlBoard();
-
-    //console.log(board);
     var msg;
-
-   // console.log(validSolution2(board));
-
-    if(totalCount(board) && validateHorizontal(board) && validateVertical(board) && validateSquares(board)){
+    if(checkSolution(board)){
         msg = 'Congratulations you completed the board!!'
     }else{
         msg = 'Your board contains errors.';
@@ -179,8 +115,6 @@ function getHtmlBoard(){
        }
        outerArray.push(innerArray);
    }
-
-//    console.log(outerArray);
     return outerArray;
 }
 
@@ -217,28 +151,6 @@ function removeBoard(){
         myNode.removeChild(myNode.firstChild);
     }
 
-}
-
-
-
-
-function equals45(n){
-    return n == 45;
-}
-
-function validSolution2(board){
-    var sumh = [0,0,0,0,0,0,0,0,0];
-    var sumv = [0,0,0,0,0,0,0,0,0];
-    osums = [[0,0,0],[0,0,0],[0,0,0]];
-    for (var i=0;i<9;i++){
-        for (var j=0;j<9;j++){
-            sumh[i] += board[i][j];
-            sumv[j] += board[i][j];
-            osums[Math.floor(i/3)][Math.floor(j/3)] += board[i][j];
-        }
-    }
-    for (var i=0;i<3;i++) if (!osums[i].every(equals45)) return false;
-    return (sumh.every(equals45) && sumv.every(equals45));
 }
 
 
